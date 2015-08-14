c1b6b725c4b81f4417e3b7c3675b12ef32be8a52
Kaelig <kaelig@deloumeau.fr>
2015-08-14 11:34:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# o-type
+
+
+
+----
+
+## Licence
+
+This software is published by the Financial Times under the [MIT licence](http://opensource.org/licenses/MIT).
---
+++ demos/src/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	"options": {
+		"sass": "demos/src/demo.scss",
+		dependencies: ["o-fonts@^2.0.0", "o-grid@^3.2.0"]
+	},
+	"demos": [
+		{
+			"name": "demo",
+			"template": "demos/src/demo.mustache"
+		}
+	]
+};
